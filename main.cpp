b105d6012dcdba68b24772728a1b130d91da6ac6
LukaszW-226014 <226014@student.pwr.edu.pl>
2016-03-31 20:49:33 +0200

    Opis zmian

--- main.cpp
+++ main.cpp
@@ -7,17 +7,39 @@ void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
-	drawCharSquare(charSquare(n), n);	
+    srand(argc == 3 ? atoi(argv[2]):time(NULL));
+	int n=atoi(argv[1]);
+	drawCharSquare(charSquare(n), n);
 }
 
 char **charSquare(int n)
 {
-	char **square;
-	return square; //hfweiuhf
+	char **square = new char *[n];
+    for ( int i = 0; i < n; ++i )
+        square[i] = new char [n];
+
+    for(int j=0;j<n;j++)
+    {
+        for(int i=0;i<n;i++)
+        {
+            square[j][i]=rand()%25+97;
+        }
+    }
+	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
+    for(int j=0;j<n;j++)
+    {
+        for(int i=0;i<n;i++)
+        {
+            printf("%c ",square[j][i]);
+        }
+        printf("\n");
+    }
 
+    for ( int i(0); i < n; ++i )
+    delete [] square[i];
+    delete [] square;
 }
